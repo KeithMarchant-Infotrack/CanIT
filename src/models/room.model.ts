6b9f350d3d812afbfb6ea51022ef87e8049c3290
dagiakatsikas--infotrack <dimitri.agiakatsikas@infotrack.com.au>
2017-07-04 09:07:45 +1000

    semi-finalised api

--- src/models/room.model.ts
+++ src/models/room.model.ts
@@ -1,7 +1,13 @@
 import { Document, Schema, Model, model } from 'mongoose';
 
 interface IRoom {
+    name: string;
+    capacity: number;
+    phone: number;
+    type: string;
     email: string;
+    location: string;
+    description: string;
 }
 
 interface IRoomModel extends IRoom, Document { }
@@ -9,10 +15,32 @@ interface IRoomModel extends IRoom, Document { }
 const roomSchema = new Schema({
     name: {
         type: String,
-        Required: ''
+        required: true,
+        unique: true
     },
     capacity: {
         type: Number,
+        min: 1,
+        max: 200
+    },
+    phone: {
+        type: Number,
+        min: 10,
+        max: 10
+    },
+    type: {
+        type: String,
+        enum: ['office', 'boardroom']
+    },
+    email: {
+        type: String,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
+    location: {
+        type: String
+    },
+    description: {
+        type: String
     }
 });
 
