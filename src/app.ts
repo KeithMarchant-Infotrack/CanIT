71fc527cdc16dd3e2fa108b809804716d2a7d3df
INFOTRACK\amber.weightman <amber.weightman@infotrack.com.au>
2017-07-03 17:36:05 +1000

    * begin implementing authentication

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 import candidateController from './controllers/candidate.controller';
+import authController from './controllers/authentication.controller';
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,10 @@ app.use(bodyParser());
 // Register controllers
 
 app.use('/candidates', candidateController);
+
+// TODO AMW this doesn't look like the right way to include a router, but i can't get it working the other way dammit
+var ac = require('./controllers/authentication.controller.js');
+app.use('/register/:userName/:email', ac.register);
 
 // Health check endpoint
 
